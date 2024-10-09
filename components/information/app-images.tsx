ef9b9332390d413a8594f621685ea0a83f809000
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-10-09 11:50:57 -0400

    :lipstick: prototype

--- components/information/app-images.tsx
+++ components/information/app-images.tsx
@@ -6,6 +6,7 @@ import ProtLocationDetailsImage from "@/public/prot-location-details.png";
 import ProtLocationCardsImage from "@/public/prot-location-flights.png";
 
 import Image from "next/image";
+import { Button } from "../ui/button";
 
 export function AppImages() {
   const pages = [
@@ -21,18 +22,30 @@ export function AppImages() {
         <h3 className="text-2xl font-semibold">App Prototype</h3>
         <hr className="border-primary w-1/12 mx-auto mt-1" />
       </div>
-      <div className="flex gap-10 justify-center">
-        {pages.map((page, index) => (
-          <div key={index} className="text-center">
-            <h4 className="text-lg font-medium mt-4">{page.title}</h4>
-            <Image
-              src={page.image}
-              alt={page.title}
-              className="rounded-lg w-[16vw] h-auto object-cover"
-              style={{ aspectRatio: "9/19.5" }}
-            />
-          </div>
-        ))}
+      <div className="space-y-5">
+        <div className="flex gap-10 justify-center">
+          {pages.map((page, index) => (
+            <div key={index} className="text-center">
+              <h4 className="text-lg font-medium mt-4">{page.title}</h4>
+              <Image
+                src={page.image}
+                alt={page.title}
+                className="rounded-lg w-[16vw] h-auto object-cover"
+                style={{ aspectRatio: "9/19.5" }}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="text-center">
+          <Button>
+            <a
+              href="https://www.figma.com/proto/kqNsumTYBQpk6BY8tJxkmZ/Royal-Trip?page-id=0%3A1&node-id=43-447&node-type=FRAME&viewport=-2623%2C1250%2C4.09&t=veMzmIr4Aa7pCtPF-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=43%3A447&share=1"
+              target="_blank"
+            >
+              View Prototype on Figma!
+            </a>
+          </Button>
+        </div>
       </div>
     </section>
   );
